5485faaab58435e1d2c29e0b3a453604149c2213
theisÃ_d <theis_d@etna-alternance.net>
2025-05-12 10:21:44 +0200

    rm .next

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_bc743cfb._.js",
-  "static/chunks/[root-of-the-server]__33bd1d78._.css"
-],
-    source: "dynamic"
-});
